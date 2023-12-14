57bf97929e8b24e04eb3f4bfd9015fe662901428
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2023-12-14 22:02:58 +0000

    Move list bind to syntax

--- src/syntax.ml
+++ src/syntax.ml
@@ -1,6 +1,7 @@
 let ( let*! ) r f = match r with Error (`Msg e) -> failwith e | Ok v -> f v
 let ( let* ) = Result.bind
 let ( >>| ) lst f = List.map f lst
+let ( >>= ) lst f = List.concat_map f lst
 
 let list_map f lst =
   let exception E of string in
